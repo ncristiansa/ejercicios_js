40576bd08a85b0708b8056167922e18a2bced073
ncristiansa <ncristiansalinasandia@gmail.com>
2019-03-28 14:37:31 +0100

    cambios

--- calculadora.php
+++ calculadora.php
@@ -1,5 +1,19 @@
 <?php
     $num1 = $_GET["num1"];
     $num2 = $_GET["num2"];
-    echo $num1+$num2;
+    $optselect = $_GET["opcion"];
+    if($optselect == "Sumar")
+    {
+        echo $num1+$num2;
+    }else if($optselect == "Dividir")
+    {
+        echo $num1/$num2;
+    }else if($optselect == "Restar")
+    {
+        echo $num1-$num2;
+    }else if($optselect == "Multiplicar")
+    {
+        echo $num1*$num2;
+    }
+    
 ?>
